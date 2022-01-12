d2d7c29696bd71e4356f51db290d439595b6a31c
Kale Leach <kleach1212@gmail.com>
2022-01-12 10:52:08 -0800

    commit tree 241ee0fc3017023fa22b69392d145ce88a483da1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+function saturdayFun(task) {
+    let satEvent = "";
+    switch(task) {
+        case 'bathe my dog':
+            satEvent =  'This Saturday, I want to bathe my dog!';
+            break;
+        default:
+            satEvent = "This Saturday, I want to roller-skate!";
+        }
+        return satEvent;
+    }
+saturdayFun('bathe my dog')
+
+const mondayWork = function(task) {
+    let monEvent = "";
+    switch(task) {
+        case "work from home":
+            monEvent = "This Monday, I will work from home.";
+            break;
+        default:
+            monEvent = "This Monday, I will go to the office.";
+        }
+        return monEvent;
+}
+
+mondayWork();
+
+function wrapAdjective(star = "*" ) {
+    return function innnerFunction(adj = "special") {
+    return `You are ${star}${adj}${star}!`;
+    }
+}
+
+wrapAdjective('*')('a hard worker');
+wrapAdjective('||')('a dedicated programmer')
